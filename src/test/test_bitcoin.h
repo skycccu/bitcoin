cfbc1e7d5af36365a412da050b92a9599277a97a
Matt Corallo <git@bluematt.me>
2017-01-19 16:49:22 -0500

    Give CMainSignals a reference to the global scheduler
    ...so that it can run some signals in the background later

--- src/test/test_bitcoin.h
+++ src/test/test_bitcoin.h
@@ -9,6 +9,7 @@
 #include "fs.h"
 #include "key.h"
 #include "pubkey.h"
+#include "scheduler.h"
 #include "txdb.h"
 #include "txmempool.h"
 
@@ -33,6 +34,7 @@ struct TestingSetup: public BasicTestingSetup {
     fs::path pathTemp;
     boost::thread_group threadGroup;
     CConnman* connman;
+    CScheduler scheduler;
 
     TestingSetup(const std::string& chainName = CBaseChainParams::MAIN);
     ~TestingSetup();
